7fdf0a19c968d4277f78e7f6018b613a3cf04c94
mjavadhm <111001770+mjavadhm@users.noreply.github.com>
2025-09-27 12:19:14 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ API_BASE_URL = 'http://89.22.236.107:9097'
 
 try:
     TOKEN = os.getenv('BOT_TOKEN')
-    api_server = TelegramAPIServer.from_base(base=API_BASE_URL)
-    session = AiohttpSession(api=api_server)
-    bot = Bot(token=TOKEN, session=session)
+    # api_server = TelegramAPIServer.from_base(base=API_BASE_URL)
+    # session = AiohttpSession(api=api_server)
+    bot = Bot(token=TOKEN)#, session=session)
 except Exception as e:
     logging.error(e)
     
